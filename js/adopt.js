2c663941dee6d574998a3c4f096071265cf63adb
Victoria Ferry <ferryvictoria@hotmail.fr>
2025-09-03 14:30:21 +0200

    Update : activation of the search button + reset of filters + link with the JSON file

--- js/adopt.js
+++ js/adopt.js
@@ -1,6 +1,47 @@
-const burgerBtn = document.getElementById('burgerBtn')
+
+/*const burgerBtn = document.getElementById('burgerBtn')
 const navLinks = document.getElementById('navLinks')
 
 burgerBtn.addEventListener('click', () => {
     navLinks.classList.toggle('active')
 })
+*/
+
+
+const btnResearch = document.getElementById('btn-research')
+
+btnResearch.addEventListener('click', async (e) => {
+    e.preventDefault()
+    const city = document.getElementById('localisation')
+    const inputTypeAnimals = document.getElementById('grid-animaux-trouvés')
+
+    let typeAnimals = choice_select()
+    console.log(typeAnimals)
+    let cityValue = city.value
+
+    let listAnimals = await animalsResearch()
+    let filteredAnimals = listAnimals.filter(animal => animal.type === typeAnimals && animal.city === cityValue)
+
+    let quantityTypeAnimalsFind = filteredAnimals.length
+    console.log(quantityTypeAnimalsFind)
+    if (quantityTypeAnimalsFind > 1){
+        inputTypeAnimals.innerText = `${quantityTypeAnimalsFind} animaux trouvés`
+    } else {
+        inputTypeAnimals.innerText = `${quantityTypeAnimalsFind} animal trouvé`
+    }
+    
+})
+
+async function animalsResearch(){
+    const response = await fetch ('/assets/animals.json')
+    const listAnimals = await response.json()
+    return listAnimals
+}
+
+function choice_select(){
+    const select = document.getElementById('select-adopt')
+    const choice = select.selectedIndex
+    const choice_selected = select.options[choice].value
+    return choice_selected
+}
+
