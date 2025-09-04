bc15db093ee26299e5c5d486f52ec7a7ce31bb2f
Juliette <jblanc17@outlook.fr>
2025-09-04 16:28:58 +0200

    fix: responsive ok

--- js/adopt.js
+++ js/adopt.js
@@ -2,30 +2,46 @@ const burgerBtn = document.getElementById('burgerBtn')
 const navLinks = document.getElementById('navLinks')
 
 burgerBtn.addEventListener('click', () => {
-    navLinks.classList.toggle('active')
+  navLinks.classList.toggle('active')
 })
 
 
 async function displayPhotosAdopt() {
   const response = await fetch("/assets/animals.json");
   const animals = await response.json();
-  const columns = [
-    document.getElementById('column1'),
-    document.getElementById('column2'),
-    document.getElementById('column3'),
-    document.getElementById('column4')
-  ]
-  
-  
 
-  animals.forEach((animal, index) => {
+
+
+  animals.forEach((animal) => {
+    const flex = document.getElementById('flex-card-adoption')
+    /* const card = document.querySelector('.card')
+     const  cardPhoto = document.querySelector('.cardPhoto')
+     const cardText = document.querySelector('.cardText')
+    */
+
     const card = document.createElement("div");
     card.classList.add("card")
+    flex.appendChild(card)
 
-    const img = document.createElement("img");
-    img.src = animal.imageUrl;
-    img.alt = animal.name;
-    img.classList.add("photo");
+
+
+    const cardPhoto = document.createElement("div");
+    cardPhoto.classList.add("cardPhoto")
+    cardPhoto.style.backgroundImage = `url(${animal.imageUrl})`;
+    cardPhoto.style.backgroundPosition = `center`
+    cardPhoto.style.backgroundSize = `cover`
+    cardPhoto.alt = animal.name;
+    card.appendChild(cardPhoto)
+
+
+   
+    const cardText = document.createElement("div");
+    cardText.classList.add("cardText")
+    card.appendChild(cardText)
+
+    const divMeet = document.createElement('div')
+    divMeet.classList.add('divMeet')
+    card.appendChild(divMeet)
 
     const type = document.createElement("p")
     type.innerText = animal.type
@@ -34,35 +50,33 @@ async function displayPhotosAdopt() {
     const h2 = document.createElement("h2")
     h2.innerText = animal.name
     h2.classList.add("name");
-    
+
     const age = document.createElement("p")
     age.innerText = animal.age
     age.classList.add("textCard");
 
-    const zipcode = document.createElement("p")
-    zipcode.innerText = animal.zipcode
-    zipcode.classList.add("textCard");
+    const city = document.createElement("p")
+    city.innerText = animal.city
+    city.classList.add("textCard");
 
     const description = document.createElement("p")
     description.innerText = animal.description
-    description.classList.add("textCard");
+    description.classList.add("descriptionCard");
 
     const meet = document.createElement("a")
     meet.innerText = "Rencontrer"
     meet.classList.add("btnMeet");
 
-     /*const columnIndex =  Math.floor(index / 2);
-     columns[columnIndex].appendChild(card);
-    */
-    const flex = document.getElementById('grid-card-adoption')
-    flex.appendChild(card)
-    card.appendChild(img);
-    card.appendChild(type);
-    card.appendChild(h2);
-    card.appendChild(age);
-    card.appendChild(zipcode);
-    card.appendChild(description);
-    card.appendChild(meet);
+
+    cardText.appendChild(type);
+    cardText.appendChild(h2);
+    cardText.appendChild(age);
+    cardText.appendChild(city);
+    cardText.appendChild(description);
+
+    divMeet.appendChild(meet);
+
+
 
   });
 }
